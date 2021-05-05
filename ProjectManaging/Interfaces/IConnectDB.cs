3e7a04df447bf5eaff9501662e0c5c5902e38c9b
Black-BKP <Black.BKP@gmail.com>
2021-05-05 16:32:20 +0700

    Add Service, Interface

---
+++ ProjectManaging/Interfaces/IConnectDB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManaging.Interfaces
+{
+    public interface IConnectDB
+    {
+        void Connect();
+    }
+}
---
+++ ProjectManaging/Services/ConnectDB.cs
@@ -0,0 +1,16 @@
+﻿using ProjectManaging.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManaging.Services
+{
+    public class ConnectDB : IConnectDB
+    {
+        public void Connect()
+        {
+            Console.WriteLine("Connect DB");
+        }
+    }
+}
