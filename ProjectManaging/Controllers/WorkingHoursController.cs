32898e4906d07c74d3a05cd8953339dc28a6acad
Black-BKP <Black.BKP@gmail.com>
2021-05-20 14:33:30 +0700

    Import form adjust

--- ProjectManaging/Controllers/WorkingHoursController.cs
+++ ProjectManaging/Controllers/WorkingHoursController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManaging.Interfaces;
+using ProjectManaging.Services;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +12,46 @@ namespace ProjectManaging.Controllers
 {
     public class WorkingHoursController : Controller
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        /*static List<JobSpentModel> jobs;*/
+        IConnectDB DB;
+
+        public WorkingHoursController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public List<string> GetJobID()
+        {
+            List<string> job_ids = new List<string>();
+            this.DB = new ConnectDB();
+            SqlConnection con = DB.Connect();
+            con.Open();
+            string str_cmd = "select Job_ID from Job";
+            SqlCommand cmd = new SqlCommand(str_cmd, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    string id = dr["Job_ID"].ToString().Trim();
+                    job_ids.Add(id);
+                }
+                dr.Close();
+            }
+            con.Close();
+            return job_ids;
+        }
+
+        [HttpGet]
+        public JsonResult GetJobNumbers()
+        {
+            return Json(GetJobID());
         }
     }
 }
