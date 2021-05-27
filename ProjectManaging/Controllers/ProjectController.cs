baccc246e3612884ac8a7ab85c77dcf5447990cf
Black-BKP <Black.BKP@gmail.com>
2021-05-27 09:23:19 +0700

    Jobs Management

--- ProjectManaging/Controllers/ProjectController.cs
+++ ProjectManaging/Controllers/ProjectController.cs
@@ -33,6 +33,42 @@ namespace ProjectManaging.Controllers
             return View();
         }
 
+        [HttpGet]
+        public JsonResult GetJobs()
+        {
+            List<JobModel> jobs = new List<JobModel>();
+            this.DB = new ConnectDB();
+            SqlConnection con = DB.Connect();
+            con.Open();
+
+            string str_cmd = "select Job_ID," +
+                                    "Job_Number, " +
+                                    "Job_Name, " +
+                                    "Estimated_Budget, " +
+                                    "Job_Year " +
+                                    "from Job";
+
+            SqlCommand cmd = new SqlCommand(str_cmd, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    JobModel job = new JobModel()
+                    {
+                        job_id = dr["Job_ID"] != DBNull.Value ? dr["Job_ID"].ToString() : "",
+                        job_number = dr["Job_Number"] != DBNull.Value ? dr["Job_Number"].ToString() : "",
+                        job_name = dr["Job_Name"] != DBNull.Value ? dr["Job_Name"].ToString() : "",
+                        estimated_budget = dr["Estimated_Budget"] != DBNull.Value ? Convert.ToInt32(dr["Estimated_Budget"]) : 0,
+                        job_year = dr["Job_Year"] != DBNull.Value ? Convert.ToInt32(dr["Job_Year"]) : 0,
+                    };
+                    jobs.Add(job);
+                }
+                dr.Close();
+            }
+            return Json(jobs);
+        }
+
         [HttpPost]
         public JsonResult AddJob(string id, string number, string name, int year)
         {
