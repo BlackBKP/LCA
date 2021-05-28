dc9d315000907552949233f9f5d2442944c9dbb5
Black-BKP <Black.BKP@gmail.com>
2021-05-28 11:08:01 +0700

    Controller Service Interface

---
+++ ProjectManaging/Interfaces/IJobSummary.cs
@@ -0,0 +1,13 @@
+﻿using ProjectManaging.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManaging.Interfaces
+{
+    interface IJobSummary
+    {
+        List<JobSummaryModel> GetJobsSummary();
+    }
+}
